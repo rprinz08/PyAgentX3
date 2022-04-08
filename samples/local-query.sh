87646a2df1161f99cc6e00f42df81c31c6af571e
rprinz08 <richard.prinz@min.at>
2022-04-08 12:38:56 +0200

    Update SNMP query script with OID text samples

--- samples/local-query.sh
+++ samples/local-query.sh
@@ -4,15 +4,21 @@
 snmpwalk -v 2c -c public localhost 1.3.6.1.4.1.8072.2.1
 echo
 
+# Values can be queried by their numeric OID as well as by their textual name.
+# Ensure that your MIBs are installed correctly!
+
 # Access node by node
 # Scalars
 
 # integer
 snmpget -v 2c -c public localhost 1.3.6.1.4.1.8072.2.1.1.0
+snmpget -v 2c -c public localhost NET-SNMP-EXAMPLES-MIB::netSnmpExampleInteger.0
 # data object
 snmpget -v 2c -c public localhost 1.3.6.1.4.1.8072.2.1.2.0
+snmpget -v 2c -c public localhost NET-SNMP-EXAMPLES-MIB::netSnmpExampleSleeper.0
 # string
 snmpget -v 2c -c public localhost 1.3.6.1.4.1.8072.2.1.3.0
+snmpget -v 2c -c public localhost NET-SNMP-EXAMPLES-MIB::netSnmpExampleString.0
 # OID
 snmpget -v 2c -c public localhost 1.3.6.1.4.1.8072.2.1.4.0
 # ip address
@@ -31,4 +37,5 @@ echo
 
 # Table
 snmptable -v 2c -c public -Ci localhost 1.3.6.1.4.1.8072.2.2.1
+snmptable -v 2c -c public -Ci localhost NET-SNMP-EXAMPLES-MIB::netSnmpIETFWGTable
 
